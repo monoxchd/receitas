69c7d59111f4184c92cb56d66e1b9c08c2e30471
Denis Neves Fróes Junior <dneves.junior@gmail.com>
2012-11-24 16:14:17 -0200

    nao sei

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,46 @@
 # encoding: utf-8
-Category.create(name: 'Acompanhamentos', slug: '')
-Category.create(name: 'Aves', slug: '')
-Category.create(name: 'Bebidas', slug: '')
-Category.create(name: 'Bolos', slug: '')
-Category.create(name: 'Carnes', slug: '')
-Category.create(name: 'Doces', slug: '')
-Category.create(name: 'Entradas', slug: '')
-Category.create(name: 'Frutos do Mar', slug: '')
-Category.create(name: 'Lanches', slug: '')
-Category.create(name: 'Massas', slug: '')
-Category.create(name: 'Molhos', slug: '')
-Category.create(name: 'Pães', slug: '')
-Category.create(name: 'Peixes', slug: '')
-Category.create(name: 'Saladas', slug: '')
-Category.create(name: 'Salgadinhos', slug: '')
-Category.create(name: 'Sobremesas', slug: '')
-Category.create(name: 'Sopas', slug: '')
-Category.create(name: 'Sorvetes', slug: '')
-Category.create(name: 'Tortas', slug: '')
-Category.create(name: 'Vegetariana', slug: '')
+# Category.create(name: 'Acompanhamentos')
+# Category.create(name: 'Aves')
+# Category.create(name: 'Bebidas')
+# Category.create(name: 'Bolos')
+# Category.create(name: 'Carnes')
+# Category.create(name: 'Doces')
+# Category.create(name: 'Entradas')
+# Category.create(name: 'Frutos do Mar')
+# Category.create(name: 'Lanches')
+# Category.create(name: 'Massas')
+# Category.create(name: 'Molhos')
+# Category.create(name: 'Pães')
+# Category.create(name: 'Peixes')
+# Category.create(name: 'Saladas')
+# Category.create(name: 'Salgadinhos')
+# Category.create(name: 'Sobremesas')
+# Category.create(name: 'Sopas')
+# Category.create(name: 'Sorvetes')
+# Category.create(name: 'Tortas')
+# Category.create(name: 'Vegetariana')
+# Recipe.create(name: "Filé de Frango ao Molho Espinafre", description: "Se o filé de frango já é gostoso, imagine com este molho.", ingredients: "-10 Filés de frango\n-Suco de 1/2 limão\n-1 colher de sopa de tempero pronto\n-sal, pimenta e manteiga a gosto\n-1 maço de espinafre cozido e espremido\n-1 colher de sopa de cebola\n-1 dente de alho\n-1 colher de sopa rasa de farinha de trigo\n-700ml de leite\n-1 caixa de creme de leite\n-Queijo ralado para polvilhar", how_to: "-Tempere os filés com o suco de limão, tempero pronto, sal e pimenta a gosto.\n-Aqueça a manteiga na frigideira e frite os filés.\n-Acomode em um refrátario e regue com o molho de espinafre.\n-Para molho de espinafre,aqueça em uma panela 2 colhes de manteiga.\n-Coloque a farinha de trigo e misture.\n-Acrescente o leite aos poucos,sempre mexendo para não empolar.\n-Junte o sal,a pimenta opcinal, o alho e a cebola.\n-A seguir,coloque o espinafre, cozido e espremido.\n-Desligue e junte o creme de leite.", category_id: '1')
+Recipe.create(name: "Virado de Frango", description: "Com um bom vinho, nossa!", ingredients: "-1 frango inteiro cortado nas juntas e levemente temperado com tempêro pronto em pó para aves e um pouco de vinagre, com 1 hora de antecedência;\n-óleo para refogar;\n-4 dentes de alho picados;\n-2 tomates picados;\n-1 colher de sopa de gengibre picadinho;\n-150 gramas de azeitonas verdes;\n-sal a gosto;\n-pimenta dedo-de-moça a gosto;\n-1 cebola grande fatiada;\n-3 ovos grandes;\n-2 xícaras de chá cheias de farinha de milho amarela;\n-salsinha picada a gosto.", how_to: "Numa panela de fundo grosso, refogar o frango no óleo já com o alho bem dourado; Fritar bem até que fique moreninho; Acrescente os tomates; em seguida as azeitonas; o gengibre; o sal e a pimenta. Deixe cozinhar bem, acrescentando água quente aos poucos. Deixe um caldo que cubra o frango; corrija o sal e a pimenta e ponha a cebola. Bata num prato os ovos levemente e ponha na panela, mexendo sempre. Assim que os ovos estiverem levemente cozidos, ponha aos poucos e rapidamente, a farinha de milho, mexendo sempre e desligue o fogo para não secar. Polvilhe com a salsinha e sirva quente.", category_id: '1')
+Recipe.create(name: "Frango com Polenta", description: "No almoço de domingo!", ingredients: "Para o frango:\n\n- ½ xícara (chá) de óleo\n- 1 kg e ½ de frango cortado em pedaços\n- Suco de 1 limão\n- 1 colher (sopa) de tempero pronto\n- 1 cebola picada\n- 250 ml de molho de tomate\n- 1 xícara (chá) de cebolinha verde\n- Sal e pimenta a gosto\n\nPara a polenta:\n\n- 2 colheres (sopa) de margarina\n- 1 dente de alho amassado\n- 1 litro de água\n- 2 xícaras (chá) de fubá\n- Sal a gosto\n- 1 caixa de creme de leite (opcional)\n- ½ xícara (chá) de queijo ralado para polvilhar", how_to: "Tempere o frango com cebola picada, tempero pronto, suco de limão, sal e pimenta. Deixe no tempero por cerca de 30 minutos para pegar gosto.\nEm uma frigideira aqueça o óleo e frite o frango. Junte o molho de tomate e cozinhe por uns 25 a 30 minutos. Retire da panela e polvilhe cebolinha.\n\nPolenta\nDerreta a margarina e refogue o alho. Adicione a água (reserve um pouco) e deixe ferver. Acrescente o fubá dissolvido com a água reservada e o sal. Cozinhe por uns 15 minutos até engrossar.\n\nA seguir, coloque em um refratário (se desejar agregue creme de leite novamente). Polvilhe queijo ralado.", category_id: '1')
+Recipe.create(name: "Frango Assado", description:"Acompanhar com batatas assadas!", ingredients: "-1 frango de 1 1/2 a 2 kilos
+\n-1 copo de suco de laranja
+\n-1 colher (sopa) de tempero pronto
+\n-2 colheres (sopa) de mostarda
+\n-50 gramas de margarina
+\n
+\n-Farofa
+\n-100 gramas de margarina
+\n-1 cebola grande
+\n-2 cenouras raladas
+\n-1/2 xícara (chá) de azeitonas verdes
+\n-Pimentão vermelho
+\n-Pimentão verde
+\n-1/2 xícara (chá) de salsinha
+\n-2 xícaras (chá) de farinha de milho
+\n-2 ovos cozidos
+\n-Sal e pimenta a gosto ", how_to: "Em um refratário coloque o frango e tempere com suco de laranja (ou abacaxi), tempero pronto, mostarde e sal (opcional) misturados previamente. Deixe por no mínimo 1 horas. A seguir, espalhe a margarina sobre o frango. Leve ao forno pré aquecido 180°C por cerca de 2 horas. Sirva com a farofa.
+\n
+\nFarofa
+\nEm uma frigideira derreta a margarina e refogue a cebola. Junte as cenouras raladas, sal e pimenta. Deixe murchar a cebola. Acrescente os pimentões verde e vermelho e azeitonas. Cozinhe por alguns minutos. Adicipne a farinha de milho, ovos cozidos picados e a salsinha. Mexa e desligue.
+\n
+\nDicas: Prenda as asas com um palito de dente e amarre as coxas com uma linha grossa. ", category_id: '1')

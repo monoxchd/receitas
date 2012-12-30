0347dbaa54ef7a855adef9d2968bf850d841ca1f
monoxchd <dneves.junior@gmail.com>
2012-12-30 07:18:19 -0200

    Finalizando o sampler da página principal

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,12 @@
 class CategoriesController < ApplicationController
 	def index
-		@categories = Category.all
+    @recipes_most_recent = Recipe.order("created_at DESC").limit(3)
+    @recipes_most_viewed = Recipe.order("views DESC").limit(3)
 	end
 
 	def show
 		@category = Category.find(params[:id])
-    @recipes = @category.recipes.order("created_at ASC")
+    @recipes = @category.recipes.order("created_at DESC")
 	end
   def viewed
     @category = Category.find(params[:id])
